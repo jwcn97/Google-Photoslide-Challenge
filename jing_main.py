bd1cc2ae5b5212a2f15729399b023dd635707bb6
Your Name <you@example.com>
2020-07-17 17:26:23 +0800

    extract_data

--- jing_main.py
+++ jing_main.py
@@ -1,18 +1,5 @@
-import pandas as pd
+from jing_util import *
 
-df = pd.read_fwf('d_pet_pictures.txt')
-# df = df[1:]
-print(df.iloc[0:2,:])
+df = extract_data("d_pet_pictures.txt")
 
-# # open file
-# f = open("d_pet_pictures.txt", "r")
-# # skips the first line (which just tells us how many pictures there are)
-# next(f)
-
-# for line in f:
-#     arr = line.split(" ", 2)
-#     arr[-1] = arr[-1][:-1]
-#     print(arr)
-#     break
-
-# f.close()
+print(compute_score(df.iloc[0,2], df.iloc[1,2]))
---
+++ jing_util.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def extract_data(filename):
+    # open file and skips the first line (which just tells us how many pictures there are)
+    f = open(filename, "r")
+    next(f)
+
+    rows = []
+    for line in f:
+        arr = line.split(" ", 2)
+        arr[-1] = arr[-1].replace('\n', '')
+        arr[-1] = set(arr[-1].split(" "))
+        rows.append(arr)
+
+    # close file
+    f.close()
+
+    # append into dataframe
+    return pd.DataFrame(rows, columns=['orientation', 'number of tags', 'tags'])
+
+'''
+pass in two sets of elements
+'''
+def compute_score(slide1, slide2):
+    intersect = slide1 & slide2
+    s1 = slide1 - slide2
+    s2 = slide2 - slide1
+    return min(len(intersect), len(s1), len(s2))
