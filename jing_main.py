6aae6a222d224e80ea8a39a02f0c824906309b9c
Your Name <you@example.com>
2020-07-17 22:23:39 +0800

    test error

--- jing_main.py
+++ jing_main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from jing_util import *
 
 # 60,000 rows of verticals
@@ -7,14 +8,5 @@ df = extract_data("d_pet_pictures.txt")
 verts = df[df['orientation'] == 'V']
 horis = df[df['orientation'] == 'H']
 
-# v_tags = [0,1,2,3,4,5,6,7,8]
-
-v_tags = verts['tags'].values
-
-print(v_tags)
-
-# res = [transition_score(v_tags(i), v_tags(j))
-#             for i in range(len(v_tags)) 
-#             for j in range(i + 1, len(v_tags))]
-
-# print(res)
+v_tags = list(verts['tags'].values)
+h_tags = list(horis['tags'].values)
