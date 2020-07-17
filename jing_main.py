2cbd34003679ec074783cfe4ca5791e8690a3d09
Your Name <you@example.com>
2020-07-17 19:48:44 +0800

    test

--- jing_main.py
+++ jing_main.py
@@ -7,4 +7,11 @@ df = extract_data("d_pet_pictures.txt")
 verts = df[df['orientation'] == 'V']
 horis = df[df['orientation'] == 'H']
 
-print(verts)
+v_tags = [0,1,2,3,4,5,6,7,8]
+
+# v_tags = verts['tags'].values
+
+res = [(i,j) for i in range(len(v_tags)) 
+          for j in range(i + 1, len(v_tags) + 1)]
+
+print(res)
