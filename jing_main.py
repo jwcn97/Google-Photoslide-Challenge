224f0fff24cdf57b2f6e01c9cd856660b372dda4
Your Name <you@example.com>
2020-07-17 19:53:55 +0800

    test

--- jing_main.py
+++ jing_main.py
@@ -7,11 +7,14 @@ df = extract_data("d_pet_pictures.txt")
 verts = df[df['orientation'] == 'V']
 horis = df[df['orientation'] == 'H']
 
-v_tags = [0,1,2,3,4,5,6,7,8]
+# v_tags = [0,1,2,3,4,5,6,7,8]
 
-# v_tags = verts['tags'].values
+v_tags = verts['tags'].values
 
-res = [(i,j) for i in range(len(v_tags)) 
-          for j in range(i + 1, len(v_tags) + 1)]
+print(v_tags)
 
-print(res)
+# res = [transition_score(v_tags(i), v_tags(j))
+#             for i in range(len(v_tags)) 
+#             for j in range(i + 1, len(v_tags))]
+
+# print(res)
