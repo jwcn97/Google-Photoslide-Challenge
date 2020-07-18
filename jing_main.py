2a738d3bee538f6902cc77646087749a311c4fc5
Your Name <you@example.com>
2020-07-18 21:30:50 +0800

    first version

--- jing_main.py
+++ jing_main.py
@@ -1,12 +1,17 @@
-import numpy as np
 from jing_util import *
 
 # 60,000 rows of verticals
 # 30,000 rows of horizontals
-df = extract_data("d_pet_pictures.txt")
+df = extract_data("test_file.txt")
+# df = extract_data("d_pet_pictures.txt")
 
-verts = df[df['orientation'] == 'V']
-horis = df[df['orientation'] == 'H']
+# verts = df[df['orientation'] == 'V']
+# horis = df[df['orientation'] == 'H']
 
-v_tags = list(verts['tags'].values)
-h_tags = list(horis['tags'].values)
+df["max"] = 0
+MAX_LEN = 10
+
+df = cal_max(df, max_len=MAX_LEN)
+print(df)
+
+optimise(0, list(df["max"]))
--- jing_util.py
+++ jing_util.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from collections import deque
+import random
 
 '''
 input: name of textfile
@@ -18,7 +20,6 @@ def extract_data(filename):
 
     # close file
     f.close()
-
     # append into dataframe
     return pd.DataFrame(rows, columns=['orientation', 'number of tags', 'tags'])
 
@@ -26,8 +27,62 @@ def extract_data(filename):
 input: two sets of string elements
 output: score between two slides
 '''
-def transition_score(slide1, slide2):
-    intersect = slide1 & slide2
-    s1 = slide1 - slide2
-    s2 = slide2 - slide1
-    return min(len(intersect), len(s1), len(s2))
+def transition_score(s1, s2):
+    return min(len(s1 & s2), len(s1 - s2), len(s2 - s1))
+
+'''
+calculate maximum score pairings
+'''
+def cal_max(df, max_len=10):
+    tags = list(df['tags'].values)
+
+    for i in range(len(tags)):
+        max_score = deque(maxlen=max_len)
+        for j in range(len(tags)):
+            if i == j: continue
+            
+            score = transition_score(tags[i], tags[j])
+            if len(max_score) == 0:
+                max_score.append((j, score))
+            else:
+                old_len = len(max_score)
+                for k in range(len(max_score),0,-1):
+                    if score > max_score[k-1][-1]:
+                        if len(max_score) == max_len:
+                            max_score.popleft()
+                            k -= 1
+                        max_score.insert(k, (j, score))
+                        break
+                if (len(max_score) < max_len) and (old_len == len(max_score)):
+                    max_score.insert(0, (j, score))
+
+        df.loc[i,"max"] = ",".join([str(x) for x in list(max_score)])
+
+    return df
+
+'''
+arrange slides according to maximum score pairings
+'''
+def optimise(start, slides):
+    new_slides = [(start,0)]
+    for i in range(len(slides)-1):
+        j = -1
+        next_slide = eval(slides[start])
+        while next_slide[j][0] in [x[0] for x in new_slides]:
+            j -= 1
+            if -j > len(next_slide): break
+        
+        if -j <= len(next_slide):
+            new_slides.append(next_slide[j])
+            start = next_slide[j][0]
+        else:
+            remaining = set(range(len(slides))) - set([x[0] for x in new_slides])
+            next_pic = random.choice(list(remaining))
+            score = transition_score(set(eval(slides[start])), set(eval(slides[next_pic])))
+            new_slides.append((next_pic, score))
+            start = next_pic
+
+        print([x[0] for x in new_slides])
+        print(sum([x[1] for x in new_slides]))
+
+
