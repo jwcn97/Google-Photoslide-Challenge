10f64edc35c0f8422d9728ca7415f857b781f454
Your Name <you@example.com>
2020-07-20 02:11:44 +0800

    optimisation done, tested up to 5000

--- jing_main.py
+++ jing_main.py
@@ -1,22 +1,23 @@
 from jing_util import *
 import time
 start_time = time.time()
+absolute_start = start_time
 
 # 60,000 rows of verticals
 # 30,000 rows of horizontals
-# df, N_pics = extract_data("d_pet_pictures.txt")
-
-# choose the first 500 pictures for experimentation
 # disregard orientation for now
 df, N_pics = extract_data("test_test.txt")
 # df, N_pics = extract_data("test_file.txt")
+clear_interval = min(1000, N_pics)
 # df, N_pics = extract_data("d_pet_pictures.txt")
-print("--- finish extracting data %s minutes ---" % ((time.time() - start_time)/60))
 print("")
+print("finish extracting data in %s ms" % ((time.time() - start_time)*1000))
+start_time = time.time()
 
 dic = cal_flow(df)
-print("--- finish creating score list %s minutes ---" % ((time.time() - start_time)/60))
+print("finish creating score dict in %s minutes" % ((time.time() - start_time)/60))
 print("")
+start_time = time.time()
 
 scr_list = sorted(dic, reverse=True)
 # find nodes with the highest score in the whole matrix
@@ -24,70 +25,64 @@ scr_list = sorted(dic, reverse=True)
 # i,j = head & tail of slideshow pointer
 new_list = [i,j]
 new_list_scr = scr_list[0]
-current_len = 0
+current_len = 1
 
-i_search = True
-j_search = True
+i_search = j_search = True
 
 # continue operation if length of slideshow is still changing
 while current_len < len(new_list):
     # update current length of new list
     current_len = len(new_list)
 
-    if i_search or j_search:
-        i_search = False
-        j_search = False
+    # remove transition scores relating to slides that already used up
+    # computationally expensive and tedious, which is why it is not run in the while loop below
+    if current_len % clear_interval == 0:
+        print("score:", new_list_scr)
+        print("--- finish creating slides %s to %s in %s minutes ---" % ((current_len-clear_interval+1), current_len, (time.time() - start_time)/60))
+        start_time = time.time()
 
+        for scr in scr_list:
+            dic[scr] = set(dic[scr]) - set([x for x in dic[scr] if (x[0] in new_list[1:-1]) or (x[1] in new_list[1:-1])])
+
+        print("--- finish clearing part %s/%s of dict in %s minutes ---" % ((current_len//clear_interval), (N_pics//clear_interval), (time.time() - start_time)/60))
+        print("")
+        start_time = time.time()
+
+    if i_search or j_search:
+        i_search = j_search = False
+
+        # loop through transitions from highest score to lowest score
         for score in scr_list:
             if not i_search:
                 # list of possible transitions at the head of the slideshow
                 lst = [x for x in dic[score] if i in x]
                 # list of possible slides appended to the head of slideshow
-                other_lst = [x[0] if i == x[1] else x[1] for x in lst]
-                remaining = [x for x in other_lst if x not in new_list]
-
+                others = [x[0] if i == x[1] else x[1] for x in lst]
+                others = [x for x in others if x not in new_list]
                 
-                for k in remaining:
-                    for scr in range(score,0,-1):
-                        dic[scr] = list(set(dic[scr]) - set([x for x in dic[scr] if i in x]))
-                    
-                    # append to start of new list and update score
-                    new_list.insert(0,k)
-                    new_list_scr += score
-                    # a slide has been found, break from the loop
-                    i_search = True
-                    # update head pointer of slideshow
-                    i = k
-                    # # remove connections that are already in the slideshow to speed up search the next round
-                    # dic[score] = list(set(dic[score]) - set(lst))
-                    break
+                if len(others) > 0:
+                    new_list.insert(0,others[0]) # append to start of new list
+                    new_list_scr += score        # update score
+                    i_search = True              # a slide has been found, break from the loop
+                    i = others[0]                # update head pointer of slideshow
 
             if not j_search:
                 # list of possible transitions at the tail of the slideshow
                 lst = [x for x in dic[score] if j in x]
                 # list of possible slides appended to the tail of slideshow
-                other_lst = [x[0] if j == x[1] else x[1] for x in lst]
-                remaining = [x for x in other_lst if x not in new_list]
+                others = [x[0] if j == x[1] else x[1] for x in lst]
+                others = [x for x in others if x not in new_list]
 
+                if len(others) > 0:
+                    new_list.append(others[0]) # append to end of new list
+                    new_list_scr += score      # update score
+                    j_search = True            # a slide has been found, break from the loop
+                    j = others[0]              # update tail pointer of slideshow
                 
-                for k in remaining:
-                    for scr in range(score,0,-1):
-                        dic[scr] = list(set(dic[scr]) - set([x for x in dic[scr] if j in x]))
-                    
-                    # append to end of new list and update score
-                    new_list.append(k)
-                    new_list_scr += score
-                    # a slide has been found, break from the loop
-                    j_search = True
-                    # update tail pointer of slideshow
-                    j = k
-                    # # remove connections that are already in the slideshow to speed up search the next round
-                    # dic[score] = list(set(dic[score]) - set(lst))
-                    break
-                
+            # both head and tails have been appended
             if i_search and j_search: break
 
-    print("score:", new_list_scr, "; list_len:", len(new_list))
+    # print("score:", new_list_scr, "; list_len:", len(new_list))
 
-print("--- finish creating slideshow %s minutes ---" % ((time.time() - start_time)/60))
+print("--- finish creating slideshow in %s hours ---" % ((time.time() - absolute)/3600))
 print("")
